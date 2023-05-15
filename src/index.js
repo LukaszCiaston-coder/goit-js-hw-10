c75feb10bed45d6e424a20662ca0d836ba1a414a
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-05-15 13:42:05 +0200

    full name country

--- src/index.js
+++ src/index.js
@@ -12,46 +12,63 @@ const DEBOUNCE_DELAY = 300;
 const handleSearch = debounce(async () => {
   const searchTerm = searchBox.value.trim();
 
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+
   if (searchTerm === '') {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
     return;
   }
 
   try {
     const countries = await fetchCountries(searchTerm);
+    const matchingCountries = countries.filter((country) =>
+      country.name.official.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-    if (countries.length > 10) {
-      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      countryList.innerHTML = '';
-      countryInfo.innerHTML = '';
-    } else if (countries.length >= 2 && countries.length <= 10) {
-      renderCountryList(countries);
-      countryInfo.innerHTML = '';
-    } else if (countries.length === 1) {
-      renderCountryList(countries[0]);
-      countryList.innerHTML = '';
+    if (matchingCountries.length === 0) {
+      Notiflix.Notify.failure('Oops, there is no country with that name.');
+    } else if (matchingCountries.length === 1) {
+      renderCountryList(matchingCountries[0]);
+    } else {
+      renderCountryList(matchingCountries);
     }
   } catch (error) {
-    if (error.message === 'Country not found') {
-      Notiflix.Notify.failure('Oops, there is no country with that name.');
-    } else {
-      Notiflix.Notify.failure('An error occurred. Please try again later.');
-    }
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
+    const errorMessage =
+      error.message === 'Country not found'
+        ? 'Oops, there is no country with that name.'
+        : 'An error occurred. Please try again later.';
+    Notiflix.Notify.failure(errorMessage);
   }
 }, DEBOUNCE_DELAY);
 
 searchBox.addEventListener('input', handleSearch);
 
+function renderCountry(country) {
+  const listItem = document.createElement('li');
+  const flagImg = document.createElement('img');
+  const countryName = document.createElement('span');
+
+  flagImg.src = country.flags.svg;
+  flagImg.alt = `${country.name.official} flag`;
+  flagImg.classList.add('flag');
+
+  countryName.textContent = country.name.official;
+
+  listItem.appendChild(flagImg);
+  listItem.appendChild(countryName);
+
+  listItem.addEventListener('click', () => {
+    displayCountryInfo(country);
+  });
+
+  countryList.appendChild(listItem);
+}
+
 function renderCountryList(countries) {
   countryList.innerHTML = '';
 
   if (!Array.isArray(countries)) {
-    // Obsługa przypadku, gdy `countries` nie jest tablicą
-    console.error('Invalid countries data');
-    return;
+    countries = [countries];
   }
 
   if (countries.length > 10) {
@@ -60,23 +77,17 @@ function renderCountryList(countries) {
   }
 
   countries.forEach((country) => {
-    const listItem = document.createElement('li');
-    const flagImg = document.createElement('img');
-    const countryName = document.createElement('span');
-
-    flagImg.src = country.flags.svg;
-    flagImg.alt = `${country.name.official} flag`;
-    flagImg.classList.add('flag');
-
-    countryName.textContent = country.name.official;
-
-    listItem.appendChild(flagImg);
-    listItem.appendChild(countryName);
-
-    listItem.addEventListener('click', () => {
-      renderCountryList(country);
-    });
-
-    countryList.appendChild(listItem);
+    renderCountry(country);
   });
+}
+
+function displayCountryInfo(country) {
+
+  countryInfo.innerHTML = `
+    <h2>${country.name.official}</h2>
+    <p>Capital: ${country.capital}</p>
+    <p>Population: ${country.population}</p>
+    <p>Languages: ${country.languages}</p>
+    <img src="${country.flags.svg}" alt="${country.name.official} flag" class="flag">
+  `;
 }
